b23c763c134d031cc3178602c47607523ebf41ce
jy0813 <75197188+jy0813@users.noreply.github.com>
2022-05-09 22:36:12 +0900

    feat: components 생성

---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>TodoList!</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+h1 {
+  color: #2f3b52;
+  font-size: 6rem;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
+</style>
